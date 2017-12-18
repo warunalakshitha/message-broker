4435c2585a083c5422424b235975d18acabe88b3
warunalakshitha <warudanu@gmail.com>
2017-12-18 18:06:14 +0530

    Fix unit tests to support authentication

--- modules/integration/src/test/java/org/wso2/messaging/integration/SuiteInitializer.java
+++ modules/integration/src/test/java/org/wso2/messaging/integration/SuiteInitializer.java
@@ -28,6 +28,11 @@ import org.testng.annotations.Parameters;
 import org.wso2.broker.amqp.Server;
 import org.wso2.broker.core.Broker;
 import org.wso2.broker.core.configuration.BrokerConfiguration;
+import org.wso2.broker.core.security.user.User;
+import org.wso2.broker.core.security.user.UserStoreManager;
+import org.wso2.broker.core.security.util.BrokerSecurityConstants;
+
+import java.io.File;
 
 public class SuiteInitializer {
     /**
@@ -38,7 +43,7 @@ public class SuiteInitializer {
     private Broker broker;
     private Server server;
 
-    @Parameters({"broker-port"})
+    @Parameters({ "broker-port" })
     @BeforeSuite
     public void beforeSuite(String port, ITestContext context) throws Exception {
         LOGGER.info("Starting broker on " + port + " for suite " + context.getSuite().getName());
@@ -49,6 +54,18 @@ public class SuiteInitializer {
         broker.startMessageDelivery();
         server = new Server(broker, configuration);
         server.start();
+
+        // set jaas.conf as system properties
+        File jaasConfig = new File(
+                this.getClass().getClassLoader().getResource(BrokerSecurityConstants.JAAS_FILE_NAME).getFile());
+        String jaasConfigPath = System
+                .setProperty(BrokerSecurityConstants.SYSTEM_PARAM_JAAS_CONFIG, jaasConfig.getAbsolutePath());
+
+        //add test user
+        User testUser = new User();
+        testUser.setUsername("admin");
+        testUser.setPassword("admin");
+        UserStoreManager.addUser(testUser);
     }
 
     @AfterSuite
